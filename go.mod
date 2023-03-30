aa7a5cb05c0b1e69934c5781912e0a65978bb48c
Aaron Russo <arusso@pixar.com>
2023-03-30 00:19:25 -0700

    initial commit

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/arusso/go-app
+
+go 1.20
+
+require github.com/arusso/go-modtest v1.0.1
+
+require github.com/arusso/go-modtest/v2 v2.0.1
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	mod1 "github.com/arusso/go-modtest"
+	mod2 "github.com/arusso/go-modtest/v2"
+)
+
+func main() {
+	mod1.Hello()
+	mod2.Hello()
+}
